30592756466ac5a1dad2c8e66cf0f4071b0462a9
namdev8 <namdevts123@gmail.com>
2024-12-03 01:13:49 -0800

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,6 @@
+def areac(r):
+    a=3.14*r*r
+    print("area of circle is :",a)
+def peric(r):
+    p=2*3.14*r
+    print("perimeter of circle is :",p)
---
+++ sphere.py
@@ -0,0 +1,6 @@
+def area(r):
+    a=4*3.14*r*r
+    print("area of sphere is ",a)
+def peris(r):
+    p=6.2832*r
+    print("perimeter of sphere is ",p)
